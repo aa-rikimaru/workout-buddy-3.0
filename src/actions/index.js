526823de613a813a8c920615943da6e5727c457c
Rikimaru <aal.malamute@gmail.com>
2018-06-01 23:21:40 -0400

    fetches all exercises from API

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,10 +5,21 @@ export const FETCH_EXERCISES = 'fetch_exercises';
 const ROOT_URL = 'https://wger.de/api/v2';
 
 export function fetchExercises() {
-  const request = axios.get(`${ROOT_URL}/exercise`);
+  let exerciseList = [];
+  const getAllExercises = (url) => {
+    if (url) {
+      axios.get(url)
+        .then((res) => {
+          exerciseList = exerciseList.concat(res.data.results);
+          getAllExercises(res.data.next);
+        });
+    }
+  }
+
+  getAllExercises(`${ROOT_URL}/exercise`);
 
   return {
     type: FETCH_EXERCISES,
-    payload: request
+    payload: exerciseList
   };
 }
--- src/reducers/reducer_exercises.js
+++ src/reducers/reducer_exercises.js
@@ -5,7 +5,7 @@ export default function(state = {}, action) {
     case FETCH_EXERCISES:
       //return state.concat([action.payload.data]);
       //return [ action.payload.data, ...state];
-      return action.payload.data.results;
+      return action.payload;
     default:
       return state;
   }
